44bd1c48ed8595f77f991fa9bf998ec4839cf383
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-08-29 15:17:17 -0700

    Refactor ESLint config for cleaner syntax

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,7 @@
 import linted from "linted";
 
-export default linted(
-  {
-    jsoncc: {
-      files: ["CodeCompletion/*.json"],
-    },
+export default linted({
+  jsoncc: {
+    files: ["CodeCompletion/*.json"],
   },
-);
+});
